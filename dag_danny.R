07f04dc916d7298708c021575b0e5fbcfedcc5b6
ChongminLee <cmlee1006@gmail.com>
2026-05-05 23:08:07 -0400

    minor revision

--- dag_danny.R
+++ dag_danny.R
@@ -3,6 +3,7 @@ library(DiagrammeRsvg)
 library(rsvg)
 library(magrittr)
 
+#Robins
 # Helper function to save a grViz object as PNG
 save_grviz_png <- function(gr, filename) {
   gr %>%
@@ -84,3 +85,104 @@ digraph dag3 {
 ")
 
 save_grviz_png(dag3, "BST258_slides/robins_dag3_postexp.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Pearl
+
+save_grviz_png <- function(gr, filename) {
+  gr %>%
+    export_svg() %>%
+    charToRaw() %>%
+    rsvg_png(filename)
+}
+
+# --------------------------------------------------
+# DAG 1: Basic mediation structure
+# X -> Z -> Y and X -> Y
+# --------------------------------------------------
+pearl_dag1_basic <- grViz("
+digraph pearl_dag1 {
+  graph [layout = dot, rankdir = LR]
+
+  node [shape = circle, style = filled, fillcolor = white, fontsize = 18]
+
+  X [label = 'X\\nExposure']
+  Z [label = 'Z\\nMediator']
+  Y [label = 'Y\\nOutcome']
+
+  X -> Z
+  Z -> Y
+  X -> Y
+}
+")
+
+save_grviz_png(pearl_dag1_basic, "BST258_slides/pearl_dag1_basic.png")
+
+
+# --------------------------------------------------
+# DAG 2: Mediation formula with covariates S
+# S blocks confounding between X and Z
+# --------------------------------------------------
+pearl_dag2_mediation_formula <- grViz("
+digraph pearl_dag2 {
+  graph [layout = dot, rankdir = LR]
+
+  node [shape = circle, style = filled, fillcolor = white, fontsize = 18]
+
+  X [label = 'X']
+  Z [label = 'Z']
+  Y [label = 'Y']
+  S [label = 'S\\nCovariates']
+
+  S -> X
+  S -> Z
+  X -> Z
+  Z -> Y
+  X -> Y
+}
+")
+
+save_grviz_png(pearl_dag2_mediation_formula, "BST258_slides/pearl_dag2_mediation_formula.png")
+
+
+# --------------------------------------------------
+# DAG 3: Path-specific effect example inspired by Pearl Figure 3
+# Heavy path: X -> Z -> W -> Y
+# Additional paths: X -> W, Z -> Y
+# --------------------------------------------------
+pearl_dag3_path_specific <- grViz("
+digraph pearl_dag3 {
+  graph [layout = dot, rankdir = LR]
+
+  node [shape = circle, style = filled, fillcolor = white, fontsize = 18]
+
+  X [label = 'X']
+  Z [label = 'Z']
+  W [label = 'W']
+  Y [label = 'Y']
+
+  X -> Z [penwidth = 3]
+  Z -> W [penwidth = 3]
+  W -> Y [penwidth = 3]
+
+  X -> W
+  Z -> Y
+}
+")
+
+save_grviz_png(pearl_dag3_path_specific, "BST258_slides/pearl_dag3_path_specific.png")
